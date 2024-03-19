ada4384b3cc51ffb38812946edba14ef0b1ebd0d
Juliendy Mendes <juliendy0708@gmail.com>
2024-03-18 20:01:49 -0400

    chore: install nuxt fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-	modules: ['@nuxtjs/tailwindcss', "@nuxt/image"],
-	tailwindcss: {
-		cssPath: '/assets/css/main.css',
-    configPath: 'tailwind.config.ts',
-	}
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxtjs/google-fonts"],
+  tailwindcss: {
+    cssPath: "/assets/css/main.css",
+    configPath: "tailwind.config.ts",
+  },
+  googleFonts: {
+    families: {
+      Montserrat: {wght: [400, 500, 600, 700]},
+    },
+  },
+});
