c22d055d24ab07823679571157544ccbd58c0f16
Juliendy Mendes <juliendy0708@gmail.com>
2024-03-26 21:11:16 -0400

    feat: initial carousel

--- app.vue
+++ app.vue
@@ -65,21 +65,31 @@
     <section
       class="bg-[#05012D] relative z-50 grid grid-cols-2 place-items-center px-12 py-20"
     >
-      <div class="relative w-0 h-0 left-[85px] border-solid border-t-0 border-r-[100px] border-b-[100px] border-l-[100px] border-t-transparent border-r-transparent border-b-[#05012D] border-l-transparent before:content-[''] before:absolute before:left-[-120px] before:top-[-12px] before:w-0 before:z-[-1] before:h-0 before:border-solid before:border-t-0 before:border-r-[120px] before:border-b-[120px] before:border-l-[120px] before:border-t-transparent before:border-r-transparent before:border-b-neutral before:border-l-transparent"></div>
-			
+      <img
+        src="/triangle.svg"
+        alt="Triangulo"
+        width="40"
+        class="absolute left-4"
+      />
+      <img
+        src="/triangle.svg"
+        alt="Triangulo"
+        width="44"
+        class="absolute top-28 right-32"
+      />
       <div>
         <NuxtImg src="/about-me.png" />
       </div>
 
       <div>
-        <h3 class="font-semibold text-2xl text-base-100 my-3">
+        <h3 class="font-semibold text-3xl text-base-100 my-3">
           Sobre
           <span
             class="bg-gradient-to-r from-neutral to-accent text-transparent bg-clip-text"
             >mim</span
           >
         </h3>
-        <p class="text-base-100 text-base w-[500px]">
+        <p class="text-base-100 text-lg w-[90%]">
           Sou apaixonada por desenvolvimento de software, tenho 2 anos de
           experiência em desenvolvimento front-end e sou capaz de criar
           interfaces de usuário atraentes e responsivas, tanto para aplicações
@@ -91,11 +101,11 @@
       </div>
     </section>
 
-    <section class="bg-[#05012D] flex px-28 gap-28 pt-14 pb-20">
+    <section class="bg-[#05012D] flex px-28 gap-28 pt-14 pb-20 relative">
       <div class="flex flex-col gap-12 basis-1/2">
         <div class="flex flex-col gap-9">
           <div>
-            <h4 class="font-semibold text-xl text-base-100">
+            <h4 class="font-semibold text-[26px] text-base-100">
               Formação
               <span
                 class="bg-gradient-to-r from-neutral to-accent text-transparent bg-clip-text"
@@ -148,7 +158,7 @@
 
       <div class="basis-1/2 flex flex-col gap-12">
         <div>
-          <h5 class="font-semibold text-xl text-base-100">
+          <h5 class="font-semibold text-[26px] text-base-100">
             Experiência
             <span
               class="bg-gradient-to-r from-neutral to-accent text-transparent bg-clip-text"
@@ -192,14 +202,78 @@
           </div>
         </div>
       </div>
+      <img
+        src="/triangle.svg"
+        alt="Triangulo"
+        width="50"
+        class="absolute bottom-24 right-56"
+      />
     </section>
 
-    <section>
-      <h6>Meu portfólio</h6>
-      <p>Projetos pessoais que desenvolvi para praticar novas habilidades.</p>
+    <section class="text-center">
+      <h6 class="font-semibold text-3xl text-base-100 my-3">
+        Meu
+        <span
+          class="bg-gradient-to-r from-neutral to-accent text-transparent bg-clip-text"
+          >portfólio</span
+        >
+      </h6>
+      <p class="font-medium text-lg text-base-100">
+        Projetos pessoais que desenvolvi para<br />
+        praticar novas habilidades.
+      </p>
 
       <div>
-        <!-- carrossel-->
+        <div className="carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img
+              src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+              className="w-full"
+            />
+            <div
+              className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+            >
+              <a href="#slide4" className="btn btn-circle">❮</a>
+              <a href="#slide2" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img
+              src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+              className="w-full"
+            />
+            <div
+              className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+            >
+              <a href="#slide1" className="btn btn-circle">❮</a>
+              <a href="#slide3" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img
+              src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+              className="w-full"
+            />
+            <div
+              className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+            >
+              <a href="#slide2" className="btn btn-circle">❮</a>
+              <a href="#slide4" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img
+              src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+              className="w-full"
+            />
+            <div
+              className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+            >
+              <a href="#slide3" className="btn btn-circle">❮</a>
+              <a href="#slide1" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
 
